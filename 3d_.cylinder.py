f1c868de0f3ce8847155b58608a7407bdaf2f635
ho1331 <60388096+ho1331@users.noreply.github.com>
2021-01-27 19:15:53 +0200

    Update and rename var_13_3.py to 3d_.cylinder.py

--- 3d_.cylinder.py
+++ 3d_.cylinder.py
@@ -8,7 +8,7 @@ import mpl_toolkits.mplot3d.art3d as art3d
 from matplotlib.patches import Circle
 
 
-# создание окна и фрейма
+# window and frame creation
 root = Tk()
 root.title('Var_13_3')
 f_top = Frame(root,relief=RIDGE,borderwidth=6)
@@ -32,7 +32,7 @@ Xc,Yc,Zc = data_for_cylinder_along_z(1,1,3,5)
 ax.plot_surface(Xc, Yc, Zc, alpha=0.5) #alpha - прозрачность
 ax.scatter(Xc,Yc,Zc, color='red')
 
-#верх\низ
+#top\bottom
 top = Circle((1, 1), 3, alpha=0.5)
 ax.add_patch(top)
 art3d.pathpatch_2d_to_3d(top, 5)
@@ -47,9 +47,9 @@ ax.set_zlabel("Z")
 
 bar1 = FigureCanvasTkAgg(fig_1, master=f_top)
 bar1.get_tk_widget().pack(side=LEFT, fill=BOTH)
+# /////////////////////////////////////////////////////////
 
-
-#поверхня
+#пsurface
 fig_2 = plt.figure()
 ax2=Axes3D(fig_2, xlim=(-5, 5), ylim=(-5, 5), zlim=(-5,5))
 bar2 = FigureCanvasTkAgg(fig_2, master=f_top)
@@ -61,7 +61,7 @@ X1,Y1=np.meshgrid(x1,y1)
 Z1 = (X1**2 + Y1**2)*np.sin(X1)*np.sin(Y1)
 ax2.plot_surface(X1,Y1,Z1,rstride=1,cstride=1,cmap=mpl.cm.hsv, alpha=0.8)
 
-# парабалоїд
+# parabaloid
 fig_3 = plt.figure()
 ax_3=Axes3D(fig_3, xlim=(-1, 1), ylim=(-1, 1), zlim=(0,3))
 bar3 = FigureCanvasTkAgg(fig_3, master=f_bottom)
@@ -74,7 +74,7 @@ Z2 = X2**2 + Y2**2
 ax_3.plot_surface(X2,Y2,Z2,rstride=1,cstride=1,cmap=mpl.cm.hsv, alpha=0.8)
 
 
-# добавление кнопки «Выход»
+# Exit_button
 def m_quit():
     root.quit()    # stops mainloop
     root.destroy() # используется в Windows
